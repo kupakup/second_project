5b98499f493d5541234128ed87ceefb1bfb5072e
Mikhail Kupavtsev2 <kupavtsev.ma@gmail.com>
2020-04-20 00:07:09 +0300

    This first commit mein

---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+var leftborderwith = 1;
+let second = 5;
+const pi = 3.14;
+console.log(second);
+
+var number = 5;
+var string = "Hello,world!";
+var sym = Symbol();
+var boolen = true;
+var fsdfasdf;
+var obj = {};
+console.log(6/6);
+console.log('string'/9);
+console.log (fsdfasdf);
+
+let persone = {
+    name: "John",
+    age: "25",
+    husband: "true"
+};
+let imgg = ["plub.png","porn.jpg","torn.bmp"];
+
+/* console.log(persone.husband);
+
+console.log(persone.name);
+
+console.log(persone["age"]);
+
+
+
+console.log (imgg[1]);
+
+let answer = confirm('Ты здесь!');
+
+console.log(answer); */
+
+/* let answer = prompt('Есть ли Вам 18 лет','Да');
+console.log(typeof(null)); */   
+/* 
+let mikhail  = 'Mikhail go in work!';
+let elena = ' Elena see serial@';
+let dima = 4234234235234523452345234568;
+console.log('mikhail'+'elena');
+console.log('dima'*45325515172345234523452345234545);
+console.log('imgg'); */
+let inc = 10,
+decr = 10;
+
+        console.log(inc++); 
+        console.log(decr--);
+
+        console.log(10%7);
+    
+        console.log('2' === 2);
+
+
+let ischeked = true;
+let isclosed = false;
+ console.log(ischeked && !isclosed);
